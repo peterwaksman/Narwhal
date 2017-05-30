1fda55ef57b5c7adc6e0c627d08eb3c17323cf26
Peter Waksman <peterwaksman@gmail.com>
2017-05-30 15:07:08 -0400

    adding stdtrees

---
+++ stdtrees/ask.py
@@ -0,0 +1,53 @@
+""" ask.py is a place to define the vocabulary of questions
+
+And anything else you can think of. Grouped as I see fit but you
+can re-arrange it or sub divide it if you like.
+
+"""
+from narwhal.nwtypes import KList
+
+# in spoken language this can be an inflection 
+kQUESTION = " ? , ask # about, question , question about , problem , need to know , want to know, help with " 
+
+kWHO = " who , who are" #asking for a person
+kHOW = " how # much|often, not sure how , do I " #asking for instructions
+kWHEN = " when " #asking for a time
+kWHERE = " where , where from , where to , where are " #asking for a place
+
+kWHAT = " what , what are, status , check , to check , verify ,\
+ ask about, ask if, to ask about " #asking for information
+
+kWHY = " why " #asking for a story
+kDOES = " do you , does it , can it " #asking about possibility
+kCAN = " can I , may I , will you , can you " # asking permission for an action
+kAMOUNT = " how much , how often , how well , how many " #asking for a quantity
+
+
+QUESTION = KList( "ask" , kQUESTION ).var()
+
+WHO = KList( "who", kWHO ).var()
+HOW = KList( "how", kHOW ).var()
+WHEN = KList( "when", kWHEN ).var()
+WHERE = KList( "where", kWHERE ).var()
+WHAT = KList( "what",kWHAT ).var()
+WHY = KList( "why", kWHY ).var()
+DOES = KList( "does", kDOES ).var()
+CAN = KList( "can", kCAN ).var()
+AMOUNT = KList("amount", kAMOUNT ).var() 
+
+QUESTION.sub(WHO)
+QUESTION.sub(HOW) 
+QUESTION.sub(WHEN)
+QUESTION.sub(WHERE) 
+QUESTION.sub(WHAT)  
+QUESTION.sub(WHY) 
+QUESTION.sub(DOES) 
+QUESTION.sub(CAN) 
+QUESTION.sub(AMOUNT)
+
+
+########################################
+########################################
+kREQUEST = ' please , use , build , fabricate , make , make me , to make , my|the|an $ order , to order , want , want to, want you to, need , need to , need you to, give me '
+REQUEST = KList( "request", kREQUEST ).var()
+
